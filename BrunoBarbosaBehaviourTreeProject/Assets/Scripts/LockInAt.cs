08ef487ee1c5080c505fbbac2225ce9ec6b986e4
dg015 <bruno.barbosa8@yahoo.com>
2025-03-26 11:55:19 -0400

    linerenderer complate

--- BrunoBarbosaBehaviourTreeProject/Assets/Scripts/LockInAt.cs
+++ BrunoBarbosaBehaviourTreeProject/Assets/Scripts/LockInAt.cs
@@ -7,9 +7,15 @@ namespace NodeCanvas.Tasks.Actions {
 	public class LockInAt : ActionTask {
 
 		public BBParameter<Transform> target;
-		//Use for initialization. This is called only once in the lifetime of the task.
-		//Return null if init was successfull. Return an error string otherwise
-		protected override string OnInit() {
+		public LineRenderer line1;
+        public LineRenderer line2;
+		public Transform barrel1;
+        public Transform barrel2;
+		public float range;
+
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+        protected override string OnInit() {
 			return null;
 		}
 
@@ -22,8 +28,9 @@ namespace NodeCanvas.Tasks.Actions {
 
 		//Called once per frame while the action is active.
 		protected override void OnUpdate() {
-			agent.transform.LookAt(target.value);
-		}
+			lockInPlayer();
+
+        }
 
 		//Called when the task is disabled.
 		protected override void OnStop() {
@@ -34,7 +41,16 @@ namespace NodeCanvas.Tasks.Actions {
 		protected override void OnPause() {
 			
 		}
+		
+		private void lockInPlayer()
+		{
+            agent.transform.LookAt(target.value);
+			line1.enabled = true;
+            line2.enabled = true;
 
+			line1.SetPosition(0, barrel1.position);
+            line1.SetPosition(1, target.value.position);
+        }
 		
 	}
 }
