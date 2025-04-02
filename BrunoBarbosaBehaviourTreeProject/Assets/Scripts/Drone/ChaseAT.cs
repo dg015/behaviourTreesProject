6ff275e23ceec0f23a2f6d540258ae62cb56d997
dg015 <bruno.barbosa8@yahoo.com>
2025-04-02 12:34:17 -0400

    zig zag formula ready for testing

--- BrunoBarbosaBehaviourTreeProject/Assets/Scripts/Drone/ChaseAT.cs
+++ BrunoBarbosaBehaviourTreeProject/Assets/Scripts/Drone/ChaseAT.cs
@@ -6,8 +6,13 @@ using UnityEngine;
 namespace NodeCanvas.Tasks.Actions {
 
 	public class ChaseAT : ActionTask {
+		//basic direction to player
 		private Vector3 direction;
 		public Transform player;
+		// zig zagging
+		private Vector3 NextLocation;
+		public float offset;
+		public float stepSize;
 
 		//Use for initialization. This is called only once in the lifetime of the task.
 		//Return null if init was successfull. Return an error string otherwise
@@ -24,10 +29,34 @@ namespace NodeCanvas.Tasks.Actions {
 
 		//Called once per frame while the action is active.
 		protected override void OnUpdate() {
-			direction = player.transform.position - agent.transform.position;
-			Debug.DrawLine(agent.transform.position, agent.transform.position+ direction);
+
+			Move();
 		}
 
+		private void Move()
+		{
+            direction = player.transform.position - agent.transform.position;
+            Debug.DrawLine(agent.transform.position, agent.transform.position + direction);
+
+            //zig zag
+
+            Vector3 adaptedStepDistance = new Vector3(offset, 0);
+
+            Vector2 baseTriangle = direction - (direction - adaptedStepDistance);
+            Vector3 NextPoint = baseTriangle / 2 + new Vector2(offset, 0);
+
+
+            Debug.DrawLine(agent.transform.position, NextPoint, Color.red);
+        }
+
+		/*if it was 2d it would be
+		 * offset is a vector 2 (0,Y)
+		 * New location = direction + offset * stepSize
+		 * Offset = -1* offset
+		 * Repeat
+		 * For 3D
+		 * 
+		*/
 		//Called when the task is disabled.
 		protected override void OnStop() {
 			
