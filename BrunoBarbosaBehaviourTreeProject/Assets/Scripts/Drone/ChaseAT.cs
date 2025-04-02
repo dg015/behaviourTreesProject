9a969e1d09173d3493e2b3f11ba60a4e6b9125a3
dg015 <bruno.barbosa8@yahoo.com>
2025-04-02 14:53:23 -0400

    zig zagging complete

--- BrunoBarbosaBehaviourTreeProject/Assets/Scripts/Drone/ChaseAT.cs
+++ BrunoBarbosaBehaviourTreeProject/Assets/Scripts/Drone/ChaseAT.cs
@@ -1,6 +1,7 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
 using UnityEngine;
+using UnityEngine.AI;
 
 
 namespace NodeCanvas.Tasks.Actions {
@@ -14,6 +15,10 @@ namespace NodeCanvas.Tasks.Actions {
 		public float offset;
 		public float stepSize;
 
+		// movement 
+		public BBParameter<NavMeshAgent> navAgent;
+
+
 		//Use for initialization. This is called only once in the lifetime of the task.
 		//Return null if init was successfull. Return an error string otherwise
 		protected override string OnInit() {
@@ -24,47 +29,46 @@ namespace NodeCanvas.Tasks.Actions {
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
-			//EndAction(true);
+			 Move();
 		}
 
 		//Called once per frame while the action is active.
 		protected override void OnUpdate() {
-			TestMove();
-			Move();
+			
+			if (navAgent.value.remainingDistance< .5)
+			{
+				Debug.Log("arrived");
+                offset = offset * -1;
+                Move();
+            }
+			
 		}
 
 
-		private void TestMove()
-		{
-
-		}
 
 		private void Move()
 		{
+			// first get the direction
             direction = player.transform.position - agent.transform.position;
             Debug.DrawLine(agent.transform.position, agent.transform.position + direction);
 
-			//zig zag
 
-			Vector3 adaptedOffset = Vector3.right * offset; // problem 
+			//zig zag
+			//adapt the offset for a vector 3 so it can be used in 3D
+			Vector3 adaptedOffset = agent.transform.right * offset; // problem 
 
 			// base triangle = player location + (direciton * offset) /2
 			Vector3 baseTriangle = agent.transform.position + direction.normalized * stepSize;
 
+			//get the mid point, that means its the top point in the triangle, that the drone will travel to
 			Vector3 midPoint = (agent.transform.position+ baseTriangle) / 2;
 
 			Vector3 NextPoint = midPoint + adaptedOffset;
+			NextLocation = NextPoint;
 
-
-
+            navAgent.value.SetDestination(NextLocation);
 
             Debug.DrawLine(agent.transform.position, NextPoint, Color.red);
-
-            if (Input.GetKey(KeyCode.Space))
-            {
-				Debug.Log("moveu");
-                offset = offset * -1;
-            }
         }
 
 		/*if it was 2d it would be
