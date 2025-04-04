a41fdb1647b92fae180f78f4254a007a42af875d
Bruno <bruno.barbosa8@yahoo.com>
2025-04-04 14:30:30 -0400

    Fixed bug

--- BrunoBarbosaBehaviourTreeProject/Assets/Scripts/Drone/DoesTurretNeedsChargingCT.cs
+++ BrunoBarbosaBehaviourTreeProject/Assets/Scripts/Drone/DoesTurretNeedsChargingCT.cs
@@ -8,7 +8,7 @@ namespace NodeCanvas.Tasks.Conditions {
         //energy
         public Blackboard Turret;
         public float TurretEnegergy;
-
+        public float LowEnergy;
 
         //Use for initialization. This is called only once in the lifetime of the task.
         //Return null if init was successfull. Return an error string otherwise
@@ -16,20 +16,35 @@ namespace NodeCanvas.Tasks.Conditions {
 			return null;
 		}
 
-		//Called whenever the condition gets enabled.
-		protected override void OnEnable() {
-			
-		}
 
-		//Called whenever the condition gets disabled.
-		protected override void OnDisable() {
-			
-		}
+        private bool CheckTurretBattery()
+        {
+            TurretEnegergy = Turret.GetVariableValue<float>("Energy");
+            if (TurretEnegergy >= LowEnergy)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
 
-		//Called once per frame while the condition is active.
-		//Return whether the condition is success or failure.
-		protected override bool OnCheck() {
-			return true;
-		}
-	}
+
+        //Called once per frame while the condition is active.
+        //Return whether the condition is success or failure.
+        protected override bool OnCheck()
+        {
+            if (CheckTurretBattery())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+    }
 }
---
+++ BrunoBarbosaBehaviourTreeProject/Assets/Scripts/Turret/DischargeAT.cs
@@ -0,0 +1,47 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine;
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class DischargeAT : ActionTask {
+		public BBParameter<float> TurretEnergy;
+		public float maxEnergy= 50;
+		public float energyDrainModifier = 1;
+		public bool isBeingCharged = false;
+
+		//Use for initialization. This is called only once in the lifetime of the task.
+		//Return null if init was successfull. Return an error string otherwise
+		protected override string OnInit() {
+			return null;
+		}
+
+		//This is called once each time the task is enabled.
+		//Call EndAction() to mark the action as finished, either in success or failure.
+		//EndAction can be called from anywhere.
+
+		//Called once per frame while the action is active.
+		protected override void OnUpdate() {
+			drain();
+		}
+
+		//Called when the task is disabled.
+		protected override void OnStop() {
+			
+		}
+
+		//Called when the task is paused.
+		protected override void OnPause() {
+			
+		}
+
+		private void drain()
+		{
+			if(TurretEnergy.value <maxEnergy || TurretEnergy.value >0 && !isBeingCharged)
+			{
+				TurretEnergy.value -= Time.deltaTime * energyDrainModifier;
+			}
+		}
+
+	}
+}
