e0ef97eeadf86c3b8b3a88ff5a96c8518994a3dd
Bruno <bruno.barbosa8@yahoo.com>
2025-04-04 14:37:28 -0400

    added energy drain and charge

--- BrunoBarbosaBehaviourTreeProject/Assets/Scripts/Drone/ChargeTurretAT.cs
+++ BrunoBarbosaBehaviourTreeProject/Assets/Scripts/Drone/ChargeTurretAT.cs
@@ -2,17 +2,24 @@ using NodeCanvas.Framework;
 using ParadoxNotion.Design;
 using UnityEngine;
 using UnityEngine.AI;
+using static UnityEngine.GraphicsBuffer;
 
 namespace NodeCanvas.Tasks.Actions {
 
 	public class ChargeTurretAT : ActionTask {
 		public NavMeshAgent navAgent;
-		public Transform turret;
+		public Transform TurretTransform;
 
+        public float TurretEnegergy;
+        public Blackboard Turret;
+		public float energyChargeModifier;
+        //flying
+        public float DefaultAirHeight = 3.61f;
+        public float ascendSpeed = 2f;
 
-		//Use for initialization. This is called only once in the lifetime of the task.
-		//Return null if init was successfull. Return an error string otherwise
-		protected override string OnInit() {
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+        protected override string OnInit() {
 			return null;
 		}
 
@@ -20,16 +27,19 @@ namespace NodeCanvas.Tasks.Actions {
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
-			if(Vector3.Distance(agent.transform.position,turret.position) > 2)
+			if(Vector3.Distance(agent.transform.position, TurretTransform.position) > 2)
 			{
+				
 
-			}
+                    TurretEnegergy += Time.deltaTime * energyChargeModifier;
+            }
 		}
 
-		//Called once per frame while the action is active.
+        //Called once per frame while the action is active.
 		protected override void OnUpdate() {
-            navAgent.SetDestination(turret.position);
-            
+            TurretEnegergy = Turret.GetVariableValue<float>("Energy");
+            navAgent.SetDestination(TurretTransform.position);
+			AirControl();
         }
 
 		//Called when the task is disabled.
@@ -41,5 +51,15 @@ namespace NodeCanvas.Tasks.Actions {
 		protected override void OnPause() {
 			
 		}
-	}
+
+        private void AirControl()
+        {
+            float speed = Mathf.Clamp(Vector3.Distance(agent.transform.position, TurretTransform.position) / ascendSpeed, .2f, 1f);
+            // if player is not being chased stay at default, if go down to player level
+            float newY = Mathf.Lerp(agent.transform.position.y, TurretTransform.position.y, Time.deltaTime * speed);
+            agent.transform.position = new Vector3(agent.transform.position.x, newY, agent.transform.position.z);
+
+        }
+
+    }
 }
