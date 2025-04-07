e6b1132b30958db103c187b63e1497c53657d4a7
Bruno <bruno.barbosa8@yahoo.com>
2025-04-07 13:39:00 -0400

    Added drone telegraphing animation

--- BrunoBarbosaBehaviourTreeProject/Assets/Scripts/Drone/AttackAT.cs
+++ BrunoBarbosaBehaviourTreeProject/Assets/Scripts/Drone/AttackAT.cs
@@ -1,6 +1,7 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace NodeCanvas.Tasks.Actions {
 
@@ -14,26 +15,50 @@ namespace NodeCanvas.Tasks.Actions {
 		public float power = 16;
 		public float radius = 10;
 
-		//Use for initialization. This is called only once in the lifetime of the task.
-		//Return null if init was successfull. Return an error string otherwise
-		protected override string OnInit() {
+		//Telegraphing
+		public GameObject barrel;
+		public Renderer BarrelRender;
+
+		//timer 
+		public float telegraphingTimer;
+        public float TimerLimit;
+
+		//navmesh
+		public NavMeshAgent navAgent;
+
+
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+        protected override string OnInit() {
 			PlayerRB = player.GetComponent<Rigidbody>();
             PlayerTransform = player.GetComponent<Transform>();
 
 
             return null;
 		}
-
-		//This is called once each time the task is enabled.
-		//Call EndAction() to mark the action as finished, either in success or failure.
-		//EndAction can be called from anywhere.
-		protected override void OnExecute() {
-			Attack();
-			EndAction(true);
-		}
 		
+		private void telegraphing()
+		{
+			telegraphingTimer = Mathf.Clamp(telegraphingTimer, 0, TimerLimit);
+			if(telegraphingTimer != TimerLimit)
+			{
+				BarrelRender.material.SetColor("_EmissionColor", Color.red);
+				telegraphingTimer += Time.deltaTime;
+			}
+			else
+			{
+				Attack();
+				EndAction(true);
+			}
 
-		private void Attack()
+		}
+
+		protected override void OnUpdate()
+		{
+			telegraphing();
+		}
+
+        private void Attack()
 		{
 			Debug.Log("attacked");
 			PlayerRB.AddExplosionForce( power,agent.transform.position,radius, 5 );	
