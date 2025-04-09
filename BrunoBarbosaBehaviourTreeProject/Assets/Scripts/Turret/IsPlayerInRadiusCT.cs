27997b46521199576b11fd0db7d182e744fc22d1
dg015 <bruno.barbosa8@yahoo.com>
2025-04-08 23:09:18 -0400

    Fixed turret bug

--- BrunoBarbosaBehaviourTreeProject/Assets/Scripts/Turret/IsPlayerInRadiusCT.cs
+++ BrunoBarbosaBehaviourTreeProject/Assets/Scripts/Turret/IsPlayerInRadiusCT.cs
@@ -9,12 +9,12 @@ namespace NodeCanvas.Tasks.Conditions {
 
         public BBParameter<Transform> target;
         public BBParameter <float> range;
-        
+        public BBParameter<bool> isCharging;
 
         //Called once per frame while the condition is active.
         //Return whether the condition is success or failure.
 		protected override bool OnCheck() {
-            if (Vector3.Distance(agent.transform.position, target.value.position) < range.value)
+            if (Vector3.Distance(agent.transform.position, target.value.position) < range.value && !isCharging.value)
             {
                 return true;
             }
